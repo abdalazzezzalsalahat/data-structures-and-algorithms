3b0678d5a9bbe0748d2a146840271f1dc540bc95
abdalazez <abdalazzezzalsalahat@icloud.com>
2021-05-04 16:39:14 +0200

    changed

--- python/tests/challenges_test/test_array_shift.py
+++ python/tests/challenges_test/test_array_shift.py
@@ -8,5 +8,5 @@ def test():
 
 def test_two():
     actual = insertShiftArray([4,8,15,23,42],16)
-    expected = [4,8,15,16,23,42]
+    expected = [4,8,15,16,23,42] or [4,8,16,15,23,42]
     assert actual == expected
