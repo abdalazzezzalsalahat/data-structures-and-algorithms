0ea26175b71c064f6f54441657230248c5497871
abdalazez <abdalazzezzalsalahat@icloud.com>
2021-05-06 23:50:22 +0200

    linked list test not completed

---
+++ python/data_structures/linked_list/linked_list.py
@@ -0,0 +1,70 @@
+class Node():
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList():
+
+    def __init__(self):
+        self.head = None
+
+    def append(self, value):
+        '''
+        A method to append a value in the last node 
+        input ==> value
+        '''
+        new_node = Node(value)
+        if not self.head:
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = new_node
+
+    def __str__(self):
+        current = self.head
+        output = ''
+        while current:
+            output += f"{ {str(current.value)} } ->"
+            current = current.next
+        return output
+
+    def insert(self, value):
+        '''
+        A method to add an node to the list 
+        input ==> element
+        '''              
+        if value is None:
+            raise  TypeError("insert() missing 1 required positional argument: 'value' ") 
+        else:
+            new_node = Node(value)
+            if not self.head:
+                self.head = new_node
+            else:
+
+                new_node = Node(value)
+                current = self.head
+                while current.next:
+                    current = current.next
+                current.next = new_node
+
+    def includes(self, values):
+          '''
+          A method to check if an element 
+          exists in the list and returns a boolean (True/Flase)
+          input ==> value 
+          output ==> boolean 
+          '''      
+          if values is None:
+            raise  TypeError("includes() missing 1 required positional argument: 'values' ") 
+          else:
+            current_node = self.head
+            while current_node.next :
+                if current_node.value == values:
+                    return True
+                else:
+                    current_node = current_node.next
+            return False
+
+        # =================================4
---
+++ python/tests/data_structures_test/test_linked_list.py
@@ -0,0 +1,66 @@
+# from data_structures.linked_list.linked_list import LinkedList
+
+from data_structures.linked_list.linked_list import (
+    LinkedList,
+)
+import pytest
+
+
+def test_import():
+    assert LinkedList
+
+def test_instance():
+    ll = LinkedList()
+    ll.insert(1)
+    ll.insert(2)
+    ll.insert(3)
+    assert isinstance(ll, LinkedList)
+
+def append() : 
+    m_m=LinkedList
+    m_m.append(2)
+    m_m.append(3)
+    m_m.append(4)
+    acepted = "{'2'} ->{'3'} ->{'4'} ->"
+    actual = m_m()
+
+def test_insert():
+    ll = LinkedList()
+    ll.insert(1)
+    ll.insert(2)
+    ll.insert(3)
+    result = ''
+    linked_list = ll.head
+    while linked_list:
+        result += f'{linked_list.value},'
+        linked_list = linked_list.next
+    assert result == '1,2,3,'
+
+def test_head():
+    ll = LinkedList()
+    ll.insert(1)
+    ll.insert(2)
+    ll.insert(3)
+    assert ll.head.value == 1
+
+def test_false():
+    ll = LinkedList()
+    ll.insert(1)
+    ll.insert(2)
+    ll.insert(3)
+    assert ll.includes(5) == False
+
+def test_true():
+    ll = LinkedList()
+    ll.insert(1)
+    ll.insert(2)
+    ll.insert(3)
+    assert ll.includes(2) == True
+
+def test_str():
+    ll = LinkedList()
+    ll.insert(1)
+    ll.insert(2)
+    ll.insert(3)
+    assert ll.__str__() == "{'1'} ->{'2'} ->{'3'} ->Null"
+
