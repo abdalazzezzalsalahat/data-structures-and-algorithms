f9a5e305f360d3d95958e2d68e6190b4521a2e65
abdalazez <abdalazzezzalsalahat@icloud.com>
2021-05-17 00:05:36 +0200

    linked list updates

--- python/linked_list/linked_list.py
+++ python/linked_list/linked_list.py
@@ -5,13 +5,12 @@ class Node():
 
 
 class LinkedList():
-
     def __init__(self):
         self.head = None
 
     def append(self, value):
         '''
-        A method to append a value in the last node 
+        this method to append value in the last node 
         input ==> value
         '''
         new_node = Node(value)
@@ -24,24 +23,18 @@ class LinkedList():
             current.next = new_node
 
     def __str__(self):
-        """
-        A method to represent a class object as a string
-        """
         current = self.head
         output = ''
         while current:
-            if current:
-                output += f"-> { {str(current.value)}} "
-                current = current.next
-            else:
-                output += "Null"
+            output += f"{ {str(current.value)} } ->"
+            current = current.next
         return output
 
     def insert(self, value):
         '''
-        A method to add an node to the list 
+        this method to adds an element to the list 
         input ==> element
-        '''              
+        '''
         if value is None:
             raise  TypeError("insert() missing 1 required positional argument: 'value' ") 
         else:
@@ -49,7 +42,6 @@ class LinkedList():
             if not self.head:
                 self.head = new_node
             else:
-
                 new_node = Node(value)
                 current = self.head
                 while current.next:
@@ -58,11 +50,11 @@ class LinkedList():
 
     def includes(self, values):
           '''
-          A method to check if an element 
+          this method to checks if an element 
           exists in the list and returns a boolean (True/Flase)
           input ==> value 
           output ==> boolean 
-          '''      
+          '''       
           if values is None:
             raise  TypeError("includes() missing 1 required positional argument: 'values' ") 
           else:
@@ -73,3 +65,74 @@ class LinkedList():
                 else:
                     current_node = current_node.next
             return False
+
+        ###########################################################################
+
+    def insert_before(self,value,new_value):
+        '''
+        adds an element to the list befor element  
+        its take to element 
+        first argument the value wich we will insert befor it 
+        the second is the value wich we will insert 
+        '''
+        new_node = Node(new_value)
+        current = self.head
+        if not self.head:
+            self.head = new_node
+        else:
+                if current.value == value:
+                    th_node = self.head
+                    self.head = new_node
+                    new_node.next = th_node
+                    return 
+                else:
+                     current = self.head
+                while current.next :
+                    if current.next.value == value:
+                        th_node = current.next
+                        current.next = new_node
+                        new_node.next = th_node
+                        return 
+                    else:
+                        current = current.next
+                return 
+
+    def insert_after(self, value, new_value):
+        '''
+        adds an element to the list after element  
+        its take to element 
+        first argument the value wich we will insert after it 
+        the second is the value wich we will insert 
+        '''
+        new_node = Node(new_value)
+        current = self.head
+        if not self.head:
+                self.head = new_node
+        else:
+            current = self.head
+            while current.next != None:
+                if current.next.value == value:
+                    current = current.next
+                    old_node = current.next
+                    current.next = new_node
+                    new_node.next = old_node
+                    return 
+                else:
+                    current = current.next
+            return
+
+    def insert_from_end(self, k):
+        """takes in a value(k) and returns the Node k places away from the tail"""
+        current = self.head
+        arr = []
+        if k < 0:
+            raise ValueError("value must be more than 0 ")
+        while current:
+            arr.append(current)
+            current = current.next
+        if len(arr) < k:
+            raise IndexError("Value extends length of List.")
+        arr.reverse()
+        if k == len(arr):
+            k = k -1
+        return arr[k].value
