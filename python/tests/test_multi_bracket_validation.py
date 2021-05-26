1be4f7c288e33690995bae3d86bbc6a32192b413
abdalazez <abdalazzezzalsalahat@icloud.com>
2021-05-26 13:05:50 +0200

    CC_13 multi brackets

---
+++ python/tests/test_multi_bracket_validation.py
@@ -0,0 +1,19 @@
+from multi_bracket_validation.multi_bracket_validation import multi_bracket_validation
+
+
+def test_multi_bracket_validation():
+    assert multi_bracket_validation('{}')
+    assert multi_bracket_validation('{}(){}')
+    assert multi_bracket_validation('{}()[[]]')
+    assert multi_bracket_validation('()')
+    assert multi_bracket_validation('(){}[]')
+    assert multi_bracket_validation('{[]}')
+    assert not multi_bracket_validation('(]')
+    assert not multi_bracket_validation('((')
+    assert not multi_bracket_validation('([)]')
+    assert not multi_bracket_validation('[({}]')
+    assert not multi_bracket_validation('{(})')
+    assert not multi_bracket_validation('{')
+    assert not multi_bracket_validation(')')
+    assert not multi_bracket_validation('[}')
+    
