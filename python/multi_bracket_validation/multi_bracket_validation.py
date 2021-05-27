010b5eb46855a887a832a7aa44ab41e9ee2c4fa9
abd alaziz <77990429+abdalazzezzalsalahat@users.noreply.github.com>
2021-05-27 16:29:56 +0200

    Update multi_bracket_validation.py

--- python/multi_bracket_validation/multi_bracket_validation.py
+++ python/multi_bracket_validation/multi_bracket_validation.py
@@ -3,11 +3,11 @@ from queue import LifoQueue
 
 def multi_bracket_validation(value: str):
     """
-    Function to check on brackets types and closings, takes a string
-    -> {we are here}
-    -> ()
-    ==> {}
-    -> []
+    The function should take a string as its only argument,
+    and should return a boolean representing whether or not the brackets in the string are balanced.
+    There are 3 types of brackets: {} [] ()
+    Given a string containing just the characters '(', ')', '{', '}', '[' and ']',
+    determine if the input string is valid.
     """
 
     if len(value) % 2 != 0:
